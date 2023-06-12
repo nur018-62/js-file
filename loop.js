a5e0bc1f07855d77fd23990722e45150a25a2298
nurulamin <admin@nurulamindig.com>
2023-06-13 01:28:22 +0600

    this is while loop

---
+++ loop.js
@@ -0,0 +1,17 @@
+console.log("while-loop");
+// make a while loop
+
+var x=0;
+while(x<50){
+ //   console.log(x);
+    x++;
+};
+
+// ramdom Number
+var x=0;
+while(x<20){
+    var y=Math.random()*99;
+    // toFixed ue to floor Number
+    console.log(y.toFixed());
+    x++;
+}
