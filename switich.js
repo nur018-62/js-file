3ddaa880ca717351d8a822a82e71a2c411ec6db2
nurulamin <admin@nurulamindig.com>
2023-06-13 17:07:03 +0600

    this is switich

---
+++ switich.js
@@ -0,0 +1,26 @@
+console.log("this is switich");
+
+var day=1;
+switch(day){
+    case 1:
+    console.log("sature day");
+    break;
+    case 2:
+    console.log("sun day");
+    break;
+    case 3:
+    console.log("mon day");
+    case 4:
+    break;
+    console.log("thuse day");
+    break;
+    case 5:
+    console.log("wednes day");
+    break;
+    case 6:
+    console.log("thus");
+    default:console.log("friday");
+
+
+
+}
